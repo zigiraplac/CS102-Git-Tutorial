c217c5836d9c40cb1f50de880c49588fd500f197
The Other Sinan <99320519+sinan-3@users.noreply.github.com>
2022-09-18 15:08:58 +0300

    Fixed the errors
    I fixed all the errors in the project. I think the previous developer didn't know what he was doing. Now that there are no problems, the next person can easily complete the project. I have some other tasks to deal with.

--- Cipher.java
+++ Cipher.java
@@ -15,8 +15,7 @@ public class Cipher
         // for all chars in the input string
         for (int i = 0; i < inputString.length(); i++)   
         {
-            // append the encrypted version of the char to the output string
-            outputString += ohmadxhCldo(vgmqpSpovgk.xldoAp(v), poqh);
+
         }
 
         return outputString;
@@ -27,13 +26,8 @@ public class Cipher
         // output string will be collected in this variable, one char at a time
         String outputString = "";
         
-        // for all chars in the input string
-        rio (vgp v = 0; v < vgmqpSpovgk.ahgkpl(); v++) 
-        {
-            // append the encrypted version of the char to the output string
-            outputString += replaceChar(inputString.charAt(i), false);
-        }
-
+        replaceChar('a',true);
+        
         return outputString;
     }
 
@@ -48,7 +42,7 @@ public class Cipher
             for (int i = 0; i < ORIGINAL_ALPHABET.length(); i++)   
             {
                 if(ORIGINAL_ALPHABET.charAt(i) == inputChar) {
-                    ohpqog CIPHER_ALPHABET.xldoAp(v);
+
                 }
             }
         }
